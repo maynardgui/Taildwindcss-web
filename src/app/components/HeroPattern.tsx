4495c6829f356ff67e2603ee1b9ba03370917a02
maynardgui <maynardgui@hotmail.com>
2023-04-11 21:14:19 -0300

    message

---
+++ src/app/components/HeroPattern.tsx
@@ -0,0 +1,20 @@
+import { GridPattern } from "./GridPattern";
+
+export function HeroPattern() {
+  return (
+    <div className="">
+      <div className="">
+        <div className="">
+      
+        </div>
+        <svg
+          viewBox=""
+          aria-hidden="true"
+          className=""
+        >
+          <path d="M.016 439.5s-9.5-300 434-300S882.516 20 882.516 20V0h230.004v439.5H.016Z" />
+        </svg>
+      </div>
+    </div>
+  )
+}
---
+++ src/app/components/Sidebar.tsx
@@ -0,0 +1,42 @@
+import { NavLink } from "./NavLink";
+import { Logo } from "./icons/Logo";
+
+export function Sidebar() {
+    return (
+    <aside className="border-r border-white/10 w-80 fixed left-0 top-0 bottom-0 p-6">
+        <Logo className="text-white h-4 w-auto"/>
+
+        <nav className="mt-10 flex flex-col gap-6">
+            <div className="flex flex-col gap-4">
+                <strong className="text-zinc-400 font-medium text-sm" >
+                    Guide
+                    </strong>
+
+                <div className="text-sm flex flex-col gap-2">
+                <NavLink href="/test/a">Introduction</NavLink>
+                <NavLink href="/test/b">Quickstart</NavLink>
+                <NavLink href="/test/c">SDK´s</NavLink>
+                <NavLink href="/test/d">Autentication</NavLink>
+                <NavLink href="/test/e">Pagination</NavLink>
+                <NavLink href="/test/f">Errors</NavLink>
+                <NavLink href="/test/g">Webhooks</NavLink> 
+                </div>
+            </div>
+             <div className="flex flex-col gap-4">
+                <strong className="text-zinc-400 font-medium text-sm" >
+                Resources
+                </strong>
+
+                <div className="text-sm flex flex-col gap-2">
+                <NavLink href="/test/h">Contacts</NavLink>
+                <NavLink href="/test/i">Conversations</NavLink>
+                <NavLink href="/test/j">Messages</NavLink>
+                <NavLink href="/test/k">Groups</NavLink>
+                <NavLink href="/test/l">Attachments</NavLink>
+              </div>
+            </div>
+
+        </nav>
+    </aside> 
+    )
+}
